ce059174ab0f45def67274dd43c057357b6b1b6c
Marcel Kubín <marcelkubin2@gmail.com>
2024-03-27 22:44:06 +0100

    Added basic Enigma structure with init and free functions

--- inc/enigma.h
+++ inc/enigma.h
@@ -1,4 +1,19 @@
 #ifndef ENIGMA_H_
 #define ENIGMA_H_
 
+#define ROTOR_COUNT 3
+#define ALPHABET_SIZE 26
+
+typedef struct {
+    unsigned char rotorSubstitute[ROTOR_COUNT][ALPHABET_SIZE];
+    unsigned char rotorInverseSubstitute[ROTOR_COUNT][ALPHABET_SIZE];
+    unsigned char reflectorSubstitute[ALPHABET_SIZE];
+    unsigned char plugboardSubstitute[ALPHABET_SIZE];
+    unsigned char ringPosition[ROTOR_COUNT];
+    unsigned char rotorPosition[ROTOR_COUNT];
+} Enigma;
+
+Enigma *enigmaInit();
+void enigmaFree(Enigma *enigma);
+
 #endif /* ENIGMA_H_ */
--- src/enigma.c
+++ src/enigma.c
@@ -1,1 +1,14 @@
 #include "../inc/enigma.h"
+#include <stdio.h>
+
+Enigma *enigmaInit() {
+    Enigma *enigma;
+    enigma = (Enigma*)malloc(sizeof(Enigma));
+    if(enigma == NULL) return NULL;
+    //TODO: Initialize other Enigma structure parts
+    return enigma;
+}
+
+void enigmaFree(Enigma *enigma) {
+    free(enigma);
+}
